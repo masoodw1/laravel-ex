eb361166e24881e294fd40e8b1393a61f33e18e4
Masood Ahmed <masoodw1@gmail.com>
2018-06-11 02:55:20 +0530

    some more changes

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'), // $dbname,
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => env('DB_HOST', '127.0.0.1'),
+            'database'  => env('DB_DATABASE', 'default'), // $dbname,
+            'username'  => env('DB_USERNAME', 'laravel'),
+            'password'  => env('DB_PASSWORD', 'computer'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
